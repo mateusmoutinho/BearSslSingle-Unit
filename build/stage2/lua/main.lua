06565c7489099527c9e30f18ca7124c0648e8077
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-09 19:52:37 -0300

    att

--- build/stage2/lua/main.lua
+++ build/stage2/lua/main.lua
@@ -1,55 +1,7 @@
-local function concat_name_on_funcs(path, content)
-    content = content:gsub("api_generator", path.get_only_name() .. "api_generator")
-    content = content:gsub("api_order", path.get_only_name() .. "api_order")
-    content = content:gsub("api_xoff", path.get_only_name() .. "api_xoff")
-    content = content:gsub("api_mul", path.get_only_name() .. "api_mul")
-    content = content:gsub("api_muladd", path.get_only_name() .. "api_muladd")
-    content = content:gsub("byteswap", path.get_only_name() .. "byteswap")
-    content = content:gsub("cswap", path.get_only_name() .. "cswap")
-    content = content:gsub("reduce_final_f255", path.get_only_name() .. "reduce_final_f255")
-
-    content = content:gsub("f255_add", path.get_only_name() .. "f255_add")
-    content = content:gsub("f255_sub", path.get_only_name() .. "f255_sub")
-    content = content:gsub("f255_mul", path.get_only_name() .. "f255_mul")
-    content = content:gsub("f255_final_reduce", path.get_only_name() .. "f255_final_reduce")
-
-    content = content:gsub("C255_R2", path.get_only_name() .. "C255_R2")
-    content = content:gsub("C255_P", path.get_only_name() .. "C255_P")
-    content = content:gsub("C255_A24", path.get_only_name() .. "C255_A24")
-    content = content:gsub("GEN", path.get_only_name() .. "GEN")
-    content = content:gsub("ORDER", path.get_only_name() .. "ORDER")
-
-    return content
-end
-
-function concat_name_in_specif_modules(path, content)
-    local MODULES = {
-        "ec_all_m15.c",
-        "ec_all_m31.c",
-        "ec_c25519_i15.c",
-        "ec_c25519_i31.c",
-        "ec_c25519_m15.c",
-        "ec_c25519_m31.c",
-        "ec_c25519_m62.c",
-        "ec_c25519_m64.c",
-        "ec_curve25519.c"
-    }
-    for i = 1, #MODULES do
-        if path.get_name() == MODULES[i] then
-            return concat_name_on_funcs(path, content)
-        end
-    end
-    return content
-end
-
 local function aply_replacment(path, content)
     content = content:gsub('#include "inner.h"', '')
     content = content:gsub('#include "config.h"', '')
     content = content:gsub('#include "bearssl.h"', '#include "../inc/bearssl.h"')
-    content = concat_name_in_specif_modules(path, content)
-
-
-
 
 
     return content
@@ -71,7 +23,11 @@ local function main()
     src.map(function(current)
         if current.path.get_extension() == "c" then
             content = aply_replacment(current.path, current.get_value())
-            current.set_value(content)
+
+            final = "namespace " .. current.path.get_only_name() .. "{\n"
+            final = final .. content .. "\n}"
+
+            current.set_value(final)
             local new_name = "fdefine." .. current.path.get_name()
             current.path.set_name(new_name)
         end

500fd48ca4baae5902a27724416e3d1fcc3841b1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-09 16:45:31 -0300

    att

--- pre_build.sh
+++ pre_build.sh
@@ -1,6 +1,12 @@
-cd build 
-gcc build/main.c -o build.o
-./build.o  
-mv final.out  ../build.out
-mv final.exe ../build.exe
-cd ..
+
+gcc build/dependencies/silverchain/src/main.c -o silverchain.out
+./silverchain.out -src buld/c \
+-tags dependencies consts types globals fdeclare fdefine 
+
+
+#cd build 
+#gcc build/main.c -o build.o
+#./build.o  
+#mv final.out  ../build.out
+#mv final.exe ../build.exe
+#cd ..
--- watch.sh
+++
@@ -1,3 +0,0 @@
-
-./silverchain.out -src buld/c \
--tags dependencies consts types globals func_declaration func_definition --watch

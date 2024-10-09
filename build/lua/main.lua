3bf8ec722ac72004fe49af756b89935d93ad231e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-09 16:26:41 -0300

    att

--- build/lua/main.lua
+++ build/lua/main.lua
@@ -1,4 +1,24 @@
 local function main()
-    print("chamou")
-end 
+    print(ANSI_BLUE .. "Downloading Bear")
+    dtw.remove_any("BearSSL")
+    os.execute("git clone https://www.bearssl.org/git/BearSSL")
+
+    local all_itens = dtw.list_files_recursively("BearSSL/src", true)
+    for i = 1, #all_itens do
+        current = all_itens[i]
+        local path_obj = dtw.newPath(current)
+        local name = path_obj.get_name()
+        local extension = path_obj.get_extension()
+        if extension == "c" then
+            local new_name = "fdefine." .. name
+            path_obj.set_only_name(new_name)
+            dtw.move_any_overwriting(current, path_obj.get_full_path())
+        end
+        if extension == "h" then
+            local new_name = "fdeclare." .. name
+            path_obj.set_only_name(new_name)
+            dtw.move_any_overwriting(current, path_obj.get_full_path())
+        end
+    end
+end
 main()

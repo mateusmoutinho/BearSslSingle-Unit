b4db7377b910786ca1d5247b4fe7304060edbcba
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-11 01:11:48 -0300

    at

---
+++ build/stage2/lua/constants.lua
@@ -0,0 +1,10 @@
+DEFAULT_JSON_MODIFIER_MODEL = [[[{
+        replacers = {},
+        private = {}
+    }]]
+MODIFIER_FOLDER             = "modifier"
+SINGLE_UNIT_FOLDER          = "BearSSLSingleUnit"
+DECLARE_NAME                = "fdeclare"
+DEFINE_NAME                 = "fdefine"
+SILVER_CHAIN_NAME           = "bearssl"
+PROVIDER_GIT                = "https://www.bearssl.org/git/BearSSL"
--- build/stage2/lua/json_replace.lua
+++ build/stage2/lua/json_replace.lua
@@ -4,3 +4,21 @@ function Aply_main_replace(main_replace_json, content)
     end
     return content
 end
+
+---@param path DtwPath
+---@retun string
+function Generate_replace_model_path(path)
+    local copy_path = dtw.newPath(path.get_full_path())
+    copy_path.replace_dirs(SINGLE_UNIT_FOLDER, MODIFIER_FOLDER)
+    return copy_path.get_full_path()
+end
+
+---@param path string
+function create_json_replace_model_if_not_exist(path)
+    if dtw.isfile(path) then
+        return
+    end
+
+
+    dtw.write_file(path, DEFAULT_JSON_MODIFIER_MODEL)
+end

7882dd135b50cd0040e46d6c0e47184c1409e3c0
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-12 13:26:06 -0300

    att

--- BearModel/imports/imports.fdefine.h
+++ BearModel/imports/imports.fdefine.h
@@ -28,8 +28,8 @@
 #include "../src/ec/fdefine.ec_c25519_m62.c"
 #include "../src/ec/fdefine.ec_c25519_m64.c"
 #include "../src/ec/fdefine.ec_curve25519.c"
-/*
 #include "../src/ec/fdefine.ec_default.c"
+/*
 #include "../src/ec/fdefine.ec_keygen.c"
 #include "../src/ec/fdefine.ec_p256_m15.c"
 #include "../src/ec/fdefine.ec_p256_m31.c"

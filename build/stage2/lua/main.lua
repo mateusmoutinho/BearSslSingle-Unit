54bbed5a17a038ea086558621650d10b9077c3e5
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-15 19:21:32 -0300

    att

--- build/stage2/lua/main.lua
+++ build/stage2/lua/main.lua
@@ -13,13 +13,13 @@ local function main()
     local single_unit_dir = dtw.concat_path(RELEASE_FODER, SINGLE_UNIT_FOLDER)
     dtw.copy_any_overwriting("BearSSL/inc", dtw.concat_path(single_unit_dir, "inc"))
     local new_src = dtw.concat_path(single_unit_dir, "src")
-    dtw.copy_any_overwriting("one.c", dtw.concat_path(single_unit_dir, "one.c"))
     silver_chain.generate_code(
         new_src,
         dtw.concat_path(single_unit_dir, "imports"),
         SILVER_CHAIN_NAME,
         { "bear", DECLARE_NAME, DEFINE_NAME }
     )
+    dtw.copy_any_merging("BearSSLModel", single_unit_dir)
     Create_summary(modifiers)
 end
 main()
